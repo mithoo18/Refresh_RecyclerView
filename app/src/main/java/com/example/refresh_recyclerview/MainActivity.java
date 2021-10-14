f07714fc100c3df01b2208b12f7563eb7cd34bf5
mithoo18 <devanshu18g@gmail.com>
2021-10-14 18:15:38 +0530

    Completed

--- app/src/main/java/com/example/refresh_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/refresh_recyclerview/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.refresh_recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.RecoverySystem;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView recyclerView;
 
+    ArrayList images = new ArrayList<>(Arrays.asList(R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background));
+    ArrayList text = new ArrayList<>(Arrays.asList("Facebook", "Twitter", "Instagram", "LinkedIn", "Youtube", "Whatsapp"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting reference of swipeRefreshLayout and recyclerView
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // Setting the layout as Linear for vertical orientation to have swipe behavior
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Sending reference and data to Adapter
+        Adapter adapter = new Adapter(MainActivity.this, images, text);
+
+        // Setting Adapter to RecyclerView
+        recyclerView.setAdapter(adapter);
+
+        // SetOnRefreshListener on SwipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+                RearrangeItems();
+            }
+        });
+    }
+
+    public void RearrangeItems() {
+        // Shuffling the data of ArrayList using system time
+        Collections.shuffle(images, new Random(System.currentTimeMillis()));
+        Collections.shuffle(text, new Random(System.currentTimeMillis()));
+        Adapter adapter = new Adapter(MainActivity.this, images, text);
+        recyclerView.setAdapter(adapter);
     }
 }
